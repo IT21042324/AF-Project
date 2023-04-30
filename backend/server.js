639e23744c9ab21fcae79ee04aade3d20daa5365
Reezan Saleem <it21042324@my.sliit.lk>
2023-04-30 19:56:39 +0530

    Added MongoDB Connection

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//Creating an express app
+const app = express();
+
+// Configure middleware function
+app.use(cors());
+
+// Get port number and database URI from environment variables
+const PORT = process.env.PORT;
+const URI = process.env.URI;
+
+// Connect to MongoDB database and start server
+mongoose
+  .connect(URI)
+  .then(() => {
+    console.log("Connection to MongoDB successful");
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+// Set up route for handling requests to /api/user endpoint
