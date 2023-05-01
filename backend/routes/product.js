41b89e5bb706a981d2ab941ee4f26bd61c16dff9
Reezan Saleem <it21042324@my.sliit.lk>
2023-05-01 16:28:46 +0530

    Added routes, controller and models for product and user

---
+++ backend/routes/product.js
@@ -0,0 +1,49 @@
+const router = require("express").Router();
+const {
+  postProduct,
+  addReview,
+  getAllProducts,
+  modifyReview,
+  deleteReview,
+  updateProduct,
+  getOneProduct,
+  deleteProduct,
+  deleteAllUserProducts,
+  addOrUpdateDiscussionThread,
+  deleteDiscussionThread,
+} = require("../controller/product");
+// Route for adding a new item
+router.post("/addProduct", postProduct);
+
+// Route for adding a new review to an item
+router.patch("/addReview", addReview);
+
+// Route for modifying an existing review for an item
+router.patch("/modifyReview", modifyReview);
+
+// Route for deleting a review for an item
+router.patch("/deleteReview", deleteReview);
+
+// Route for deleting an item
+router.delete("/deleteProduct/:id", deleteProduct);
+
+// Route for getting all items
+router.get("/", getAllProducts);
+
+// Route for getting a specific item by ID
+router.get("/:id", getOneProduct);
+
+// Route for updating an item
+router.patch("/updateProduct", updateProduct);
+
+// Route for deleting all items for a specific store by store ID
+router.delete("/deleteStoreProducts/:id", deleteAllUserProducts);
+
+//Route for starting a new discussion for a specific product
+router.patch("/discussion/addOrUpdateDiscussion", addOrUpdateDiscussionThread);
+
+//Route to delete the discussion thread
+router.patch("/discussion/deleteDiscussion", deleteDiscussionThread);
+
+//export all routes
+module.exports = router;
