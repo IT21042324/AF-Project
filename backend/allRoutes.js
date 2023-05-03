689570424867ab57d260ab7f74b49c8b0f8d865c
Reezan Saleem <it21042324@my.sliit.lk>
2023-05-03 21:28:29 +0530

    Added the routes to be protected

--- backend/allRoutes.js
+++ backend/allRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("./routes/user");
 const productRouter = require("./routes/product");
+const protectedProuductRouter = require("./routes/protectedProduct");
 const eventRouter = require("./routes/events");
 const hotelsRouter = require("./routes/hotels");
 const roomsRouter = require("./routes/rooms");
@@ -9,7 +10,8 @@ const placeRouter = require("./routes/place.route");
 
 // Set up route for handling requests to /api/user endpoint
 router.use("/api/users", userRouter);
-router.use("/api/products", productRouter);
+router.use("/api/normal/products", productRouter);
+router.use("/api/protected/products", protectedProuductRouter);
 router.use("/api/events", eventRouter);
 router.use("/api/hotels", hotelsRouter);
 router.use("/api/rooms", roomsRouter);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,58 +1,11 @@
 const router = require("express").Router();
-const {
-  postProduct,
-  addReview,
-  getAllProducts,
-  modifyReview,
-  deleteReview,
-  updateProduct,
-  getOneProduct,
-  deleteProduct,
-  deleteAllUserProducts,
-  addOrUpdateDiscussionThread,
-  deleteDiscussionThread,
-  incrementLikeCounter,
-  decrementLikeCounter,
-} = require("../controller/product");
-
-// Route for adding a new item
-router.post("/addProduct", postProduct);
-
-// Route for adding a new review to an item
-router.patch("/addReview", addReview);
-
-// Route for modifying an existing review for an item
-router.patch("/modifyReview", modifyReview);
-
-// Route for deleting a review for an item
-router.patch("/deleteReview", deleteReview);
-
-// Route for deleting an item
-router.delete("/deleteProduct/:id", deleteProduct);
+const { getAllProducts, getOneProduct } = require("../controller/product");
 
 // Route for getting all items
 router.get("/", getAllProducts);
 
 // Route for getting a specific item by ID
 router.get("/:id", getOneProduct);
-
-// Route for updating an item
-router.patch("/updateProduct", updateProduct);
-
-// Route for deleting all items for a specific store by store ID
-router.delete("/deleteStoreProducts/:id", deleteAllUserProducts);
-
-//Route for starting a new discussion for a specific product
-router.patch("/discussion/addOrUpdateDiscussion", addOrUpdateDiscussionThread);
-
-//Route to delete the discussion thread
-router.patch("/discussion/deleteDiscussion", deleteDiscussionThread);
-
-//Route to increse the like counter
-router.patch("/incrementLikeCounter", incrementLikeCounter);
-
-//Route to decrese the like counter
-router.patch("/decrementLikeCounter", decrementLikeCounter);
 
 //export all routes
 module.exports = router;
