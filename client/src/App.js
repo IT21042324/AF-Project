ed2e74532a7908d8ce6baaf882584516c7fad738
Arani12 <it21014536@my.sliit.lk>
2023-05-03 23:25:43 +0530

    check commit

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,8 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { BaseRoutes } from "./BaseRoutes";
 import { EntrepreneurshipRoutes } from "./EntrepreneurshipRoutes";
-
+import { AddEvent } from "./pages/culturalEvents/addEvent";
+import EditInfo from "./pages/culturalEvents/editEvent";
 function App() {
   return (
     <div className="App">
@@ -12,6 +13,8 @@ function App() {
           path="/entrepreneurship/*"
           element={<EntrepreneurshipRoutes />}
         />
+        <Route path="/addEvent" element={<AddEvent />} />
+        <Route path="/editEvent" element={<EditInfo />} />
       </Routes>
     </div>
   );
