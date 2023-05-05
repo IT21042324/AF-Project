4ba7bd51f0312a421ecb9e8eed2e2fe320f2c5b2
Arani12 <it21014536@my.sliit.lk>
2023-05-05 22:25:41 +0530

    commit  3

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { BaseRoutes } from "./BaseRoutes";
 import { EntrepreneurshipRoutes } from "./EntrepreneurshipRoutes";
+import { AddEvent } from "./pages/culturalEvents/addEvent";
+import EditInfo from "./pages/culturalEvents/editEvent";
+import DisplayEvents from "./pages/culturalEvents/displayEvents";
+import BookTicket from "./pages/culturalEvents/BookEvent";
 import { AdminRoutes } from "./AdminRoutes";
 import { AccommodationRoutes } from "./AccommodationRoutes";
 import { PlaceRoutes } from "./PlaceRoutes";
@@ -15,6 +19,10 @@ function App() {
           path="/entrepreneurship/*"
           element={<EntrepreneurshipRoutes />}
         />
+        <Route path="/addEvent" element={<AddEvent />} />
+        <Route path="/editEvent" element={<EditInfo />} />
+        <Route path="/displayEvents" element={<DisplayEvents />} />
+        <Route path="/BookEvent" element={<BookTicket />} />
         <Route path="/admin/*" element={<AdminRoutes />} />
 
         {/* Accommodation Routes */}
